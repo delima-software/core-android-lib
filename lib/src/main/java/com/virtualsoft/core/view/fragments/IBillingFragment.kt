f9895b0e47f4403684186a3bcadae6b68339815a
Abner Lima <abner.lima27@outlook.com>
2020-06-20 19:07:49 -0300

    removed warnPremium

--- lib/src/main/java/com/virtualsoft/core/view/fragments/IBillingFragment.kt
+++ lib/src/main/java/com/virtualsoft/core/view/fragments/IBillingFragment.kt
@@ -3,6 +3,4 @@ package com.virtualsoft.core.view.fragments
 interface IBillingFragment {
 
     fun checkPremium(callback: (Boolean) -> Unit) { }
-
-    fun warnPremium(isPremium: Boolean, callback: (Boolean) -> Unit) { }
 }
