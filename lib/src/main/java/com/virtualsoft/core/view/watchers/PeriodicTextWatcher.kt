8fe325b340d083483eca384269a7005a1ee0c9fb
Abner Lima <abner.lima27@outlook.com>
2020-11-23 09:02:27 -0300

    periodic text watcher

--- lib/src/main/java/com/virtualsoft/core/view/watchers/PeriodicTextWatcher.kt
+++ lib/src/main/java/com/virtualsoft/core/view/watchers/PeriodicTextWatcher.kt
@@ -2,15 +2,32 @@ package com.virtualsoft.core.view.watchers
 
 import android.text.Editable
 import android.text.TextWatcher
+import com.virtualsoft.core.designpatterns.builder.IBuild
+import com.virtualsoft.core.designpatterns.builder.IBuilder
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import java.util.*
 
-class PeriodicTextWatcher(val listener: () -> Unit): TextWatcher {
+class PeriodicTextWatcher(val listener: () -> Unit): TextWatcher, IBuild {
 
     private var timer: Timer? = null
-    private val DELAY: Long = 1000
+    private var DELAY: Long = 1000
+    private var LENGTH: Int = 5
+
+    class Builder(val listener: () -> Unit): IBuilder<PeriodicTextWatcher> {
+        override val building = PeriodicTextWatcher(listener)
+
+        fun setDelay(timeInMillis: Long): Builder {
+            building.DELAY = timeInMillis
+            return this
+        }
+
+        fun setEditableLength(length: Int): Builder {
+            building.LENGTH = length
+            return this
+        }
+    }
 
     override fun beforeTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
@@ -22,7 +39,7 @@ class PeriodicTextWatcher(val listener: () -> Unit): TextWatcher {
 
     override fun afterTextChanged(s: Editable?) {
         s?.let { editable ->
-            if (editable.length > 5) {
+            if (editable.length > LENGTH) {
                 timer = Timer()
                 timer?.schedule(object: TimerTask() {
                     override fun run() {
