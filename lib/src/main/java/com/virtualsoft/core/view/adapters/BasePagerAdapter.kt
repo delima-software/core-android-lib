de67379d2bdfb857b670369b9e46587ca434abe9
Abner Lima <abner.lima27@outlook.com>
2020-11-30 22:25:05 -0300

    add icons to base view pager adapter

--- lib/src/main/java/com/virtualsoft/core/view/adapters/BasePagerAdapter.kt
+++ lib/src/main/java/com/virtualsoft/core/view/adapters/BasePagerAdapter.kt
@@ -1,6 +1,7 @@
 package com.virtualsoft.core.view.adapters
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
@@ -8,6 +9,7 @@ abstract class BasePagerAdapter(val context: Context, val fragment: Fragment) : 
 
     protected var size = 0
     protected val tabTitles = mutableListOf<Int>()
+    protected val tabIcons = mutableListOf<Int>()
 
     override fun getItemCount(): Int {
         return size
@@ -16,6 +18,13 @@ abstract class BasePagerAdapter(val context: Context, val fragment: Fragment) : 
     fun getPageTitle(position: Int): CharSequence? {
         tabTitles.getOrNull(position)?.let { stringRes ->
             return context.resources.getString(stringRes)
+        }
+        return null
+    }
+
+    fun getPageIcon(position: Int): Drawable? {
+        tabIcons.getOrNull(position)?.let { iconRes ->
+            return context.getDrawable(iconRes)
         }
         return null
     }
